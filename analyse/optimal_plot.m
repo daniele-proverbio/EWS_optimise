eff3269c3176e724638c691736596cf24a7f29ad
Daniele Proverbio <47269114+daniele-proverbio@users.noreply.github.com>
2022-06-28 09:36:58 +0200

    Add files via upload

---
+++ analyse/optimal_plot.m
@@ -0,0 +1,43 @@
+%% Plot optimal weights
+% Results are saved after each run of "full_optimise.m", for different
+% alpha
+
+alpha = 0:0.1:1;
+
+w =  [0, 0, 1 ;
+       0, 0, 1 ;
+       0.1, 0.2, 0.7;
+       0.4, 0.3, 0.3;
+       0.1, 0.2, 0.6
+       0.6, 0.4, 0;
+       0.6, 0.3, 0.1;
+       0.7, 0.2, 0.1;
+       1,0,0
+       0.7, 0.1, 0.2;
+       0.9, 0.1, 0]';
+
+w_bis = [0,0,1;
+         0.1, 0, 0.9;
+         0, 0.2, 0.8;
+         0.2, 0.4, 0.4;
+         0.2, 0.3, 0.5;
+         0.6, 0.4, 0;
+         0.4, 0.4, 0.2;
+         0.7, 0.3, 0;
+         1,0,0;
+         0.7,0.1,0.2;
+         0.9,0.1,0]';
+
+figure()
+hold on
+plot(alpha,w_bis(1,:),'-o',linewidth=1.5,color=[0, 0.4470, 0.7410])
+plot(alpha,w(1,:),'--o',linewidth=1.5,color=[0, 0.4470, 0.7410])
+plot(alpha,w_bis(2,:),'-o',linewidth=1.5,color=[0.8500, 0.3250, 0.0980])
+plot(alpha,w(2,:),'--o',linewidth=1.5,color=[0.8500, 0.3250, 0.0980])
+plot(alpha,w_bis(3,:),'-o',linewidth=1.5,color=[0.9290, 0.6940, 0.1250])
+plot(alpha,w(3,:),'--o',linewidth=1.5,color=[0.9290, 0.6940, 0.1250])
+ax = gca;
+ax.FontSize = 16; 
+ylabel('$\mathbf{\hat{w}}$',fontsize=26,Interpreter='latex')
+xlabel('$\alpha$',fontsize=26,Interpreter='latex')
+legend('Var','','AC(1)','','H_S','Position',[0.754464285714286 0.419047619047619 0.136607142857143 0.151190476190476],fontsize=18) 
